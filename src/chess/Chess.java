9d8665f8b56a937d525e31df7dba07d16f4782c5
youssefhanna03 <118490606+youssefhanna03@users.noreply.github.com>
2024-02-11 19:30:53 -0500

    finished start method

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -101,11 +101,6 @@ public class Chess {
 	for (int i = 0; i <8; i++){
 		board[6][i].pieceType = ReturnPiece.PieceType.BP;
 	}
-	for (int i = 2; i <6; i++){
-		for (int j = 0; j <8; j++){
-			board[i][j].pieceType = null;
-		}
-	}
 		/* FILL IN THIS METHOD */
 }
 
